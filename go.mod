fc3b534fa7d4aa3446d9e6328b8269e580cc2352
Shreyash <hello@webdevcaptain.com>
2024-04-03 09:37:10 +0530

    Implement basic auth functionality in auth package.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/webdevcaptain/KeyGuardian-Go
 
 go 1.22.1
+
+require golang.org/x/crypto v0.21.0
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,31 @@
 // Authentication
+package auth
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Username string
+	Password []byte
+}
+
+func CreateUser(username, password string) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %v", err)
+	}
+
+	user := &User{
+		Username: username,
+		Password: hashedPassword,
+	}
+
+	return user, nil
+}
+
+func AuthenticateUser(user *User, password string) bool {
+	err := bcrypt.CompareHashAndPassword(user.Password, []byte(password))
+	return err == nil
+}
